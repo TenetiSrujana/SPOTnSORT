34bceec4d2d8cfd92edcd74123d1318db5b2a5a5
TenetiSrujana <srujanateneti@gmail.com>
2026-02-15 17:12:31 +0530

    sync full project frontend and backend updated code

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 // backend/routes/auth.js
 import express from "express";
 import bcrypt from "bcryptjs";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 const router = express.Router();
 
